f0585cd7011805db6442df3f93fbf0b710907697
Bryan D'Haeseleer <Bryan.DHaeseleer@bpostbanque.be>
2021-11-23 11:30:47 +0100

    feat(): Rename potentialNeighbor() to getNeighbors() with the appropriate logic in order to get the correct number of the neighbors (and not the "potential" number)

--- GoL/GoL/Models/Cell.swift
+++ GoL/GoL/Models/Cell.swift
@@ -21,14 +21,45 @@ struct Cell {
     let yPos: Int
     var state: CellState
 
-    init(xPos: Int, yPos:Int) {
+    init(xPos: Int, yPos: Int) {
         self.xPos = xPos
         self.yPos = yPos
         self.state = .dead
     }
 
+    /**
+     This function will return an array of index representing the potential neighbor for a given index and a given size
 
-    func potentialNeighbor(at index: Int, for size: GridSize) -> [Int] {
-        return [Int]()
+     ```
+     Neighboor position
+     ### index-(size+1) ### - ### index-size ### - ### index-(size-1) ###
+     ### index-1        ######### CELL  ########   ### index+1 ###
+     ### index+(size-1) ### - ### index+size ### - ### index(size+1)  ###
+     ```
+     */
+    func getNeighbors(at index: Int, for gridSize: GridSize) -> [Int] {
+        let maxSize = gridSize.maxSize
+
+        guard index >= 0 && index < gridSize.maxSize else {
+            return []
+        }
+
+        // ex: grid size 10x10 and index 10%10 will give us true
+        let isLeftEdge = index % gridSize.size == 0
+        // ex: grid size 10x10 and index 99+1%10 will give us true
+        let isRightEdge = (index + 1) % gridSize.size == 0
+        let upper = index-gridSize.size
+        let downstair = index + gridSize.size
+
+        let potentialNeighbors = [isRightEdge ? -1: upper+1,
+                                  upper,
+                                  isLeftEdge ? -1 : upper-1,
+                                  isLeftEdge ? -1 : index-1,
+                                  isRightEdge ? -1 : index+1,
+                                  isLeftEdge ? -1 : downstair-1,
+                                  downstair,
+                                  isRightEdge ? -1: downstair+1
+        ].filter({$0 >= 0 && $0 < maxSize})
+        return potentialNeighbors
     }
 }
--- GoL/GoLTests/CellTests.swift
+++ GoL/GoLTests/CellTests.swift
@@ -25,18 +25,18 @@ class CellTests: XCTestCase {
         let gridSize = GridSize(10)
 
         // Wrong index
-        XCTAssertEqual(cell.potentialNeighbor(at: -1, for: gridSize).count, 0)
-        XCTAssertEqual(cell.potentialNeighbor(at: 100, for: gridSize).count, 0)
+        XCTAssertEqual(cell.getNeighbors(at: -1, for: gridSize).count, 0)
+        XCTAssertEqual(cell.getNeighbors(at: 100, for: gridSize).count, 0)
 
         // top left and right edge
-        XCTAssertEqual(cell.potentialNeighbor(at: 0, for: gridSize).count, 3)
-        XCTAssertEqual(cell.potentialNeighbor(at: 9, for: gridSize).count, 3)
+        XCTAssertEqual(cell.getNeighbors(at: 0, for: gridSize).count, 3)
+        XCTAssertEqual(cell.getNeighbors(at: 9, for: gridSize).count, 3)
         // bottom left and right edge
-        XCTAssertEqual(cell.potentialNeighbor(at: 90, for: gridSize).count, 3)
-        XCTAssertEqual(cell.potentialNeighbor(at: 99, for: gridSize).count, 3)
+        XCTAssertEqual(cell.getNeighbors(at: 90, for: gridSize).count, 3)
+        XCTAssertEqual(cell.getNeighbors(at: 99, for: gridSize).count, 3)
         // middle bottom
-        XCTAssertEqual(cell.potentialNeighbor(at: 95, for: gridSize).count, 5)
+        XCTAssertEqual(cell.getNeighbors(at: 95, for: gridSize).count, 5)
         // middle of somewhere
-        XCTAssertEqual(cell.potentialNeighbor(at: 55, for: gridSize).count, 8)
+        XCTAssertEqual(cell.getNeighbors(at: 55, for: gridSize).count, 8)
     }
 }
