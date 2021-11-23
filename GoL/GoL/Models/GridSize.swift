a4282cc4a1f719e270cff85cf99ea6de392892f9
Bryan D'Haeseleer <Bryan.DHaeseleer@bpostbanque.be>
2021-11-23 10:34:38 +0100

    test(GOL): Implement test cases for Grid

---
+++ GoL/GoL/Models/GridSize.swift
@@ -0,0 +1,16 @@
+//
+//  GridSize.swift
+//  GoL
+//
+//  Created by Bryan D'Haeseleer on 23/11/2021.
+//
+
+import Foundation
+
+struct GridSize {
+    let size: Int
+
+    init(_ size: Int) {
+        self.size = size
+    }
+}
